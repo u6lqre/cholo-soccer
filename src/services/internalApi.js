2a3a33bbd04fcc141f1bb1d7fc4fd4dd75a2a597
u6lqre <ozwtn9@tutamail.com>
2025-02-24 19:20:12 +0100

    schedule external API call at 02:00 everyday

--- src/services/internalApi.js
+++ src/services/internalApi.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cron from "node-cron";
 import { getLaLigaMatches } from "./externalApi.js";
 
 const app = express();
 const port = 3000;
 
-const matches = await getLaLigaMatches();
+let matches;
+
+cron.schedule("0 2 * * *", async () => {
+  console.log("Fetching matches at 2:00...");
+  matches = await getLaLigaMatches();
+});
 
 app.get("/laliga", async (req, res) => {
   res.json(matches);
